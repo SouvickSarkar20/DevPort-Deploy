9be05bd909d4b3d2644b65bdc376ee7eb6747e41
SouvickSarkar20 <souvicksarkarcbsa@gmail.com>
2025-10-31 15:55:16 +0530

    final

--- reverse-proxy/src/index.ts
+++ reverse-proxy/src/index.ts
@@ -2,16 +2,29 @@ import express, { Request, Response } from "express";
 import httpProxy, { ProxyReqCallback } from "http-proxy";
 
 const app = express();
-const PORT = 8000
+const PORT = 8000;
 
-const BASE_PATH = "https://devport-deploy.s3.ap-south-1.amazonaws.com/__outputs"
+const BASE_PATH =
+    "https://devport-deploy.s3.ap-south-1.amazonaws.com/__outputs";
 
 const proxy = httpProxy.createProxy();
 
 app.use((req: Request, res: Response) => {
     const hostname = req.hostname;
-    const subdomain = hostname.split(".")[0]
+    const subdomain = hostname.split(".")[0];
 
-    const resolveTo = `${BASE_PATH}/${subdomain}`;
-    proxy.web(req, res, { target: resolveTo, changeOrigin: true });
-})
+    const resolvesTo = `${BASE_PATH}/${subdomain}`;
+
+    proxy.web(req, res, { target: resolvesTo, changeOrigin: true });
+});
+
+proxy.on("proxyReq", (proxyReq, req, res) => {
+    const url = (req as Request).url;
+    if (url === "/") {
+        proxyReq.path = (proxyReq.path || "") + "index.html";
+    }
+});
+
+app.listen(PORT, () =>
+    console.log(`Reverse Proxy Running on port ${PORT}..`)
+);
